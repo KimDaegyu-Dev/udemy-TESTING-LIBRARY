b166401c2dd8920e4e578dd61f59d7cd4d4602f0
KimDaegyu <ddk3207@naver.com>
2025-04-03 04:34:56 +0900

    16

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import React from "react";
 function App() {
+  const [buttonColor, setButtonColor] = React.useState("red");
+  const nextColor = buttonColor === "red" ? "blue" : "red";
   return (
     <>
-      <button className="red">change to blue</button>
+      <button className={buttonColor} onClick={() => setButtonColor(nextColor)}>
+        change to {nextColor}
+      </button>
     </>
   );
 }
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,6 +1,26 @@
-import { render, screen } from "@testing-library/react";
-import { logRoles } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+// import { logRoles } from "@testing-library/react";
 import App from "./App";
+test("button starts with correct label and color", () => {
+  const { container } = render(<App />);
+  // logRoles(container);
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  // check initial button label
+  expect(buttonElement).toHaveClass("red");
+});
+
+test("button has correct label and color after click", () => {
+  //render app
+  render(<App />);
+  //find the button
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+
+  //click the button
+  fireEvent.click(buttonElement);
+  // check the button label and color
+  expect(buttonElement).toHaveTextContent(/red/i);
+  expect(buttonElement).toHaveClass("blue");
+});
 
 // test("App contains correct heading", () => {
 //   render(<App />);
@@ -21,13 +41,3 @@ import App from "./App";
 //     throw new Error("test error");
 //   }).toThrow("test error");
 // });
-
-test("button starts with correct color", () => {
-  const { container } = render(<App />);
-  logRoles(container);
-  const buttonElement = screen.getByRole("button", { name: /blue/i });
-  expect(buttonElement).toHaveClass("red");
-});
-
-test("button has correct color after click", () => {});
-test("button has correct text after click", () => {});
