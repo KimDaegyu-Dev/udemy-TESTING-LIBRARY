c317a13b2a72d00dbf896a7d3bd138557f2c210d
KimDaegyu <ddk3207@naver.com>
2025-04-03 02:51:32 +0900

    5

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -6,3 +6,11 @@ test("App contains correct heading", () => {
   const headingElement = screen.getByText(/learn react/i);
   expect(headingElement).toBeInTheDocument();
 });
+
+test("empty test", () => {});
+
+test("test throws error explicitly", () => {
+  expect(() => {
+    throw new Error("test error");
+  }).toThrow("test error");
+});
