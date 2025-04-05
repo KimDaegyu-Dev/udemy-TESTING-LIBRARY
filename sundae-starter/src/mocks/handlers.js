1cdc233300fcaa991cac3f255e4f06cb0ce59c5b
KimDaegyu <ddk3207@naver.com>
2025-04-05 12:48:17 +0900

    44

---
+++ sundae-starter/src/mocks/handlers.js
@@ -0,0 +1,14 @@
+import { http, HttpResponse } from "msw";
+
+export const handlers = [
+  http.get("http://localhost:3030/scoops", () => {
+    // JSON으로 stringify 할 필요 없음
+    return HttpResponse.json({
+      scoops: [
+        { name: "Chocolate", imagePath: "/images/chocolate.png" },
+        { name: "Vanilla", imagePath: "/images/vanilla.png" },
+        { name: "Strawberry", imagePath: "/images/strawberry.png" },
+      ],
+    });
+  }),
+];
--- sundae-starter/src/setupTests.js
+++ sundae-starter/src/setupTests.js
@@ -1,1 +1,13 @@
 import "@testing-library/jest-dom";
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./mocks/node";
+
+// establish API mocking before all tests.
+beforeAll(() => server.listen());
+
+// reset any request hnadlers that we may add during the tests
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+
+// clean up after the tests are finished.
+afterAll(() => server.close());
